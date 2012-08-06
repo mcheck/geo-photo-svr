5000a6d923e857225fbf5948da9b528ce268f79d
mcheck <mcheck@sologroup.com>
2012-08-05 22:33:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoPhotoSvr::Application.config.secret_token = '4383442e77237b98892ccfb594f2163b779bc7623b0da9e91d882dda9a63d8d6769833638a7af6ffce04a07d4841138a066f01e0a8d36636d64913dbd514f426'
